ae6c6917e83e2f4d79ebd2cfbd6e9ddb7d41a08f
Magaca78 <mateo.1701920666@ucaldas.edu.co>
2022-03-01 22:09:52 -0500

    primer crud en loopback

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './product.controller';
+export * from './order.controller';
+export * from './person.controller';
+export * from './pedido-product.controller';
